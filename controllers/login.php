c1bb544fbde0077cf66047f840fd4e0896e13e00
Tan Nguyen <minhtan1707@gmail.com>
2018-11-03 19:29:40 +0700

    no message

--- controllers/login.php
+++ controllers/login.php
@@ -1,27 +1,34 @@
 <?php
 $server_name=$_SERVER['SERVER_NAME'];
 session_start();
-if($_POST['email'])
+if($_SESSION['user_info']['login'] == 1)
 {
-    require_once './models/member.php';
-    //kiểm tra thông tin login
-    $email=$_POST['email'];
-    $password=$_POST['password'];
-    $login=login($email,$password);
-    // print_r($login);
-    if($login)
-    {
-        //nếu đúng thông tin cho $_SESSION['user_info']['login'] = 1
-        
-        $_SESSION['user_info']=$login;
-        $_SESSION['user_info']['login']=1;
-        header("location: dashboard.php");
-    }else{
-        //sai thông tin trả lại về trang login
-
-        header("location: index.php?act=login");
-    }
-}else{  
-    require_once './views/login.php';
+    header("location: dashboard.php");
 }
+else{
+    if($_POST['email'])
+    {
+        require_once './models/member.php';
+        //kiểm tra thông tin login
+        $email=$_POST['email'];
+        $password=$_POST['password'];
+        $login=login($email,$password);
+        // print_r($login);
+        if($login)
+        {
+            //nếu đúng thông tin cho $_SESSION['user_info']['login'] = 1
+            
+            $_SESSION['user_info']=$login;
+            $_SESSION['user_info']['login']=1;
+            header("location: dashboard.php");
+        }else{
+            //sai thông tin trả lại về trang login
+    
+            header("location: index.php?act=login");
+        }
+    }else{  
+        require_once './views/login.php';
+    }
+}
+
 ?>
